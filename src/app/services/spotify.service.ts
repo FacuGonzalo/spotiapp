6de83aafd7b6c732171e928e97fb7f899d05caae
Facu <gonzalofacu@gmail.com>
2021-06-26 12:08:22 -0300

    Inicio

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor( private http: HttpClient ) {
+
+   }
+
+  getNewReleases(){
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQDsnU8IhPdDKrUXuWq7FVMlrNmZrmMJiSyG6fRESINYgAJBBpnLiQ6GThLMuQsxqlLJVavErt4qQAgzvrA'
+    });
+
+    this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
+    .subscribe(data => {
+      console.log(data)
+    })
+  }
+
+}
